4ad957027ff10a1b83f5ca06a72429a7c9881d03
Exel Tarkus <exel.tarkus@adira.co.id>
2022-08-02 13:02:49 +0700

    mos4862 - cloudinary config

---
+++ src/app/config/cloudinary_config.ts
@@ -0,0 +1,27 @@
+import dotenv from 'dotenv';
+import { v2 as cloudinary } from 'cloudinary'
+// var cloudinary = require('cloudinary').v2
+
+dotenv.config();
+
+const cloudinaryConfig = {
+  cloudinary_path : process.env.CLOUDINARY_PATH,
+	cloudinary_cloud_name : process.env.CLOUDINARY_CLOUD_NAME,
+	cloudinary_api_key : process.env.CLOUDINARY_API_KEY,
+	cloudinary_api_secret : process.env.CLOUDINARY_API_SECRET
+}
+
+cloudinary.config({
+  cloud_name: cloudinaryConfig.cloudinary_cloud_name,
+  api_key: cloudinaryConfig.cloudinary_api_key,
+  api_secret: cloudinaryConfig.cloudinary_api_secret,
+  secure: true,
+});
+
+const cloudinaryBaseUrl = `https://res.cloudinary.com/adiramoservice/q_auto,f_auto/v1/${cloudinaryConfig.cloudinary_path}`
+
+export {
+  cloudinary,
+  cloudinaryConfig,
+  cloudinaryBaseUrl
+}
